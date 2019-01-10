0685e12f480dbd081b06cac19c066985de969b3e
paulo <paulo.vescovi@viasoft.com.br>
2019-01-10 17:50:45 -0200

    dsfdsfdf

--- app/src/main/java/paulo/com/br/bico/ui/activity/MainActivity.kt
+++ app/src/main/java/paulo/com/br/bico/ui/activity/MainActivity.kt
@@ -1,13 +1,55 @@
 package paulo.com.br.bico.ui.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import paulo.com.br.bico.R
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appBar.setOutlineProvider(null)
+
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.nav_rules) {
+            Toast.makeText(applicationContext, "Vakıf Kurallarını Seçtiniz.", Toast.LENGTH_SHORT).show()
+
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(applicationContext, "Galeriyi seçtiniz.", Toast.LENGTH_SHORT).show()
+
+        } else if (id == R.id.nav_contact) {
+            Toast.makeText(applicationContext, "Bize Yazın seçtiniz.", Toast.LENGTH_SHORT).show()
+
+        }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
